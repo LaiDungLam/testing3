41e0fcb2fe2a62efe07fb2e7f8f3bd60b6805681
Lam Lai <laidunglam@gmail.com>
2020-06-08 06:25:40 +0700

    hey, this is a great way to manage code versions. since there might be so many changes to the code and the details level is really high, need a way to manage this all. And Git Hub has a greate machenism

---
+++ test3-first.R
@@ -0,0 +1,4 @@
+hey man: 
+  - I have linked a project in Git hubt down to my R Studio 
+- Now I made some changes in the project at my local computer, and then I want 
+to push them back to the Github
